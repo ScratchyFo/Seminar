21fe47e523452d5a6f4fc568d5cd25b59941a3dd
Vitaliy <vitali_wf@mail.ru>
2023-02-06 03:56:21 +0700

    ДЗ - Четвертое задание

---
+++ Csharp/Homework/4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+
+if (a > b) {
+    max = a;
+}  
+if (b > c) {
+    max = b;
+}
+if (c > max) {
+    max = c;
+}
+Console.WriteLine($"Максимальное число {max}");
+
