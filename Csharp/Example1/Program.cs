2145fa7cfd1795ab2c2d8514e826d0794f1bf78e
Vitaliy <vitali_wf@mail.ru>
2023-02-06 00:31:45 +0700

    Второе задание

--- Csharp/Example1/Program.cs
+++ Csharp/Example1/Program.cs
@@ -3,8 +3,18 @@
 Console.Clear();
 
 Console.WriteLine("Введите число: ");
+
+// При вводе 99999 - по методу sqr ответ некорректен.
 int number = int.Parse(Console.ReadLine()!);
 int sqr = number * number;
+
+// При вводе 99999 - по методу sqr ответ некорректен.
+// Int32 number = int.Parse(Console.ReadLine()!);
+// Int32 sqr = number * number;
+
+// При вводе 99999 - ответ стабилен вроде как.
+// Int64 number = int.Parse(Console.ReadLine()!);
+// Int64 sqr = number * number;
 double sqr2 = Math.Pow(number, 2);
 
 Console.WriteLine($"Квадрат числа {number} равен {sqr}");
