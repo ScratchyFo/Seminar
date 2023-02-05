c586d0f4dc538aa204522c00c45631442dd05a96
Vitaliy <vitali_wf@mail.ru>
2023-02-05 20:32:16 +0700

    Первое задание

---
+++ Csharp/Example1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа возведения числа в квадрат.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int sqr = number * number;
+double sqr2 = Math.Pow(number, 2);
+
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+Console.WriteLine($"Квадрат числа {number} равен {sqr2}");
