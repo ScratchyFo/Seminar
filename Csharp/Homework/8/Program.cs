d5a9f8c860651fa3beb02d28d2dd09b2f6af4787
Vitaliy <vitali_wf@mail.ru>
2023-02-06 04:35:45 +0700

    ДЗ - Задание 8

---
+++ Csharp/Homework/8/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int k = 2;
+while (k <= N)
+{
+    Console.Write($"{k} ");
+    k=k + 2;
+}
