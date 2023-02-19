bc174c54be4e3be831bf945ffc724d90d1cebe5c
Vitaliy <vitali_wf@mail.ru>
2023-02-20 05:50:46 +0700

    Exmp 10

---
+++ Csharp/Example 10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает 2  числа и проверяет явялется ли оно квадартом числа другого
+Console.Clear();
+
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine()!);
+
+
+if ( a * a == b || b * b == a ) {
+    Console.WriteLine("Является квадратом");
+} else {
+    Console.WriteLine("Не является квадратом");
+}
