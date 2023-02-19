ccf2057f68732a62b6b87e4bd5037008cc07d996
Vitaliy <vitali_wf@mail.ru>
2023-02-20 05:12:04 +0700

    Example 12

---
+++ Csharp/Example8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 12. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+// Если число 2 н екратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4; 16, 4 -> кратно;
+
+Console.Clear();
+
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine()!);
+
+int result = a % b;
+
+if (result == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else {
+    Console.WriteLine($"Не кратно, остаток {result}");
+}
+
+
