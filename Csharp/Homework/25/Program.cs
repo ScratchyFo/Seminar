68429b5d5880d394416ee68d3ae5b77a8efc599c
Vitaliy <vitali_wf@mail.ru>
2023-02-27 23:12:27 +0700

    Homework 25 27 29

---
+++ Csharp/Homework/25/Program.cs
@@ -0,0 +1,23 @@
+﻿/// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+int PowerMatch(int A, int B)
+{
+    int res = 1;
+    for (int i = 0; i < B; i++)
+    {
+        res *= A;
+    }
+    return res;
+}
+
+Console.Write("Введите возводимое: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите степень: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Число {a} в степени {b} = {PowerMatch(a, b)}");
---
+++ Csharp/Homework/29/Program.cs
@@ -0,0 +1,32 @@
+﻿//// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33-> [6, 1, 33]
+
+Console.Clear();
+
+Console.Write("Введите минимальное число: ");
+int min = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите максимальное число: ");
+int max = int.Parse(Console.ReadLine()!);
+
+int[] getArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void printArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+
+    Console.Write($"{inArray[i]} ");
+}
+
+
+int[] array = getArray();
+printArray(array);
