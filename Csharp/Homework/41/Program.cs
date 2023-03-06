39c703b8de8ec02330c4692aefa95b5e86a34e80
Vitaliy <vitali_wf@mail.ru>
2023-03-06 16:54:55 +0700

    41 43

---
+++ Csharp/Homework/41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int[] getArray(int size, int min, int max)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(min, max+1);
+    }
+    return res;
+}
+
+int getPositive(int[] array)
+{
+    int result = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+Console.Write("Введите количество чисел: ");
+
+int size = int.Parse(Console.ReadLine()!);
+int[] array = getArray(size, -99, 99);
+
+Console.WriteLine(String.Join(", ", array));
+
+if (getPositive(array) == 0)
+{
+    Console.Write($"Чисел больше 0 ннет ");
+}
+else
+{
+    Console.Write($"Чисел больше 0 => {getPositive(array)}");
+}
---
+++ Csharp/Homework/43/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку
+// пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+double X(int b1, int k1, int b2, int k2)
+{
+    double x1 = (b2 - b1);
+    double x2 = (k1 - k2);
+    double x = x1 / x2;
+    return x;
+}
+
+double Y(int b1, int k1, double x)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+
+Console.Write("Введите точку B1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите точку K1: ");
+int k1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите точку B2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите точку K2: ");
+int k2 = int.Parse(Console.ReadLine()!);
+
+double x = X(b1, k1, b2, k2);
+double y = Y(b1, k1, x);
+
+Console.Write($"Точка пересечения ({x:f1}; {y:f1})");
