80742b42d8dc8916705fb5f0cec0820c1ff8e7d2
Vitaliy <vitali_wf@mail.ru>
2023-03-11 17:29:19 +0700

    56 58

---
+++ Csharp/Homework/56/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int[,] getArray(int a, int b, int min, int max)
+
+{
+    int[,] res = new int[a, b];
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            res[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+
+    return res;
+}
+
+void printArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void arraySum (int[,] array2D)
+{
+    int line = 0;
+    int sumMin = Int32.MaxValue;
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            sum = sum + array2D[i, j];
+        }
+        if (sum < sumMin)
+        {
+            sumMin = sum;
+            line++;
+        }
+        
+    }
+    Console.Write($"Наименьшая сумма у строки {line}");
+}
+
+Console.Clear();
+
+int[,] array2D = getArray(4, 3, 0, 9);
+printArray(array2D);
+
+Console.WriteLine();
+
+arraySum(array2D);
---
+++ Csharp/Homework/58/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] getMatrix(int a, int b, int min, int max)
+{
+    int[,] matrix = new int[a, b];
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void printMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] multipMatrix(int[,] mA, int[,] mB)
+{
+    int[,] mC = new int[mA.GetLength(0), mB.GetLength(1)];
+    if (mA.GetLength(1) != mB.GetLength(0))
+    {
+        Console.WriteLine("Ошибка умножения мматриц");
+        return mC;
+    }
+    for (int i = 0; i < mC.GetLength(0); i++)
+    {
+        for (int j = 0; j < mC.GetLength(1); j++)
+        {
+            for (int k = 0; k < mA.GetLength(1); k++)
+            {
+                mC[i, j] += mA[i, k] * mB[k, j];
+            }
+
+        }
+    }
+    return mC;
+}
+
+Console.Clear();
+
+Console.Write("Введите матрицу A через запятую: ");
+int[] dimA = Array.ConvertAll(Console.ReadLine()!.Split(','), Convert.ToInt32);
+int[,] mA = getMatrix(dimA[0], dimA[1], 0, 9);
+
+Console.Write("Введите матрицу B через запятую: ");
+int[] dimB = Array.ConvertAll(Console.ReadLine()!.Split(','), Convert.ToInt32);
+int[,] mB = getMatrix(dimB[0], dimB[1], 0, 9);
+
+printMatrix(mA);
+Console.WriteLine();
+printMatrix(mB);
+
+int[,] mC = multipMatrix(mA, mB);
+Console.WriteLine();
+printMatrix(mC);
