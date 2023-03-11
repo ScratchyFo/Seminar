9d61560341aeb91b84add4357b4906889712e12c
Vitaliy <vitali_wf@mail.ru>
2023-03-11 18:35:46 +0700

    54 56 58 60 62

--- Csharp/Homework/62/Program.cs
+++ Csharp/Homework/62/Program.cs
@@ -5,41 +5,43 @@
 // 11 16 15 06
 // 10 09 08 07
 
-int[,] FillArraySpiral(int m, int n)
+int[,] fillArraySpiral(int a, int b)
 {
+    int[,] array = new int[a, b];
     int s = 1;
-    int[,] array = new int[m, n];
+    
+    // Наполнение массива по часовой
 
-    //Заполняем периметр массива по часовой стрелке.
-    for (int y = 0; y < n; y++)
+    for (int y = 0; y < b; y++)
     {
         array[0, y] = s;
         s++;
     }
-    for (int x = 1; x < m; x++)
+
+    for (int x = 1; x < a; x++)
     {
-        array[x, n - 1] = s;
+        array[x, b - 1] = s;
         s++;
     }
-    for (int y = n - 2; y >= 0; y--)
+
+    for (int y = b - 2; y >= 0; y--)
     {
-        array[m - 1, y] = s;
+        array[a - 1, y] = s;
         s++;
     }
-    for (int x = m - 2; x > 0; x--)
+
+    for (int x = a - 2; x > 0; x--)
     {
         array[x, 0] = s;
         s++;
     }
 
-    //Периметр заполнен. Продолжаем заполнять массив и задаём
-    //координаты ячейки, которую необходимо заполнить следующей.
     int c = 1;
     int d = 1;
 
-    while (s < m * n)
+    while (s < a * b)
     {
-        //Движемся вправо.
+        // Вправо
         while (array[c, d + 1] == 0)
         {
             array[c, d] = s;
@@ -47,7 +49,7 @@ int[,] FillArraySpiral(int m, int n)
             d++;
         }
 
-        //Движемся вниз.
+        // Вниз
         while (array[c + 1, d] == 0)
         {
             array[c, d] = s;
@@ -55,7 +57,7 @@ int[,] FillArraySpiral(int m, int n)
             c++;
         }
 
-        //Движемся влево.
+        // Влево
         while (array[c, d - 1] == 0)
         {
             array[c, d] = s;
@@ -63,19 +65,18 @@ int[,] FillArraySpiral(int m, int n)
             d--;
         }
 
-        //Движемся вверх.
+        // Вверх
         while (array[c - 1, d] == 0)
         {
-            array[c, d] = s;
+            s = array[c, d];
             s++;
             c--;
         }
     }
-    //При данном решении в центре всегда остаётся незаполненная ячейка.
-    //Убираем её при помощи следующего цикла.
-    for (int x = 0; x < m; x++)
+  
+    for (int x = 0; x < a; x++)
     {
-        for (int y = 0; y < n; y++)
+        for (int y = 0; y < b; y++)
         {
             if (array[x, y] == 0)
             {
@@ -87,7 +88,7 @@ int[,] FillArraySpiral(int m, int n)
 }
 
 
-void PrintArray(int[,] array)
+void printArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -110,5 +111,5 @@ int m = int.Parse(Console.ReadLine()!);
 Console.Write("Введите ширину массива: ");
 int n = int.Parse(Console.ReadLine()!);
 
-int[,] array2D = FillArraySpiral(m, n);
-PrintArray(array2D);
+int[,] array2D = fillArraySpiral(m, n);
+printArray(array2D);
