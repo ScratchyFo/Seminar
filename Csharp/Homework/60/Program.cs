428285ae9b7f0723c4a8961339aa9db0944f0f1c
Vitaliy <vitali_wf@mail.ru>
2023-03-11 18:22:11 +0700

    60

---
+++ Csharp/Homework/60/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] create3DArr()
+{
+    int[,,] array = new int[2, 2, 2];
+    int[] values = new int[89];
+    int num = 10;
+    for (int i = 0; i < values.Length; i++)
+    {
+        values[i] = num++;
+    }
+
+    for (int i = 0; i < values.Length; i++)
+    {
+        int randomIndex = new Random().Next(0, values.Length);
+        int temp = values[i];
+        values[i] = values[randomIndex];
+        values[randomIndex] = temp;
+    }
+
+    int valueIndex = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = values[valueIndex++];
+            }
+        }
+    }
+    return array;
+}
+
+void printArray(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                Console.Write($"{inArray[i, j, k]} ({i}, {j}, {k})\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] array3D = create3DArr();
+printArray(array3D);
