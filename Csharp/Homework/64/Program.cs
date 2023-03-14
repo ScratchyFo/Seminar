f49e7a41a2ae6c88f1b9e4adad31b4fd6c2ef990
Vitaliy <vitali_wf@mail.ru>
2023-03-14 14:06:55 +0700

    64

---
+++ Csharp/Homework/64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int count = 1;
+string printNumbers(int n, int count)
+
+{
+    if (count == n) return $"{n}";
+    return printNumbers(n, count + 1) + $", {count}";
+}
+
+Console.Clear();
+Console.Write("Введите число N: ");
+
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(printNumbers(n, count));
