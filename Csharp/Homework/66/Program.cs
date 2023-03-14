f1d75e59c80ab0f2598703228575c3b500ee8516
Vitaliy <vitali_wf@mail.ru>
2023-03-14 14:14:22 +0700

    66

---
+++ Csharp/Homework/66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int sumElements(int m, int n)
+{
+    int res = m;
+    if (m == n) return m;
+    m++;
+    res = res + sumElements(m, n);
+    return res;
+}
+
+Console.Write("Введите значение M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма натуральных элементов: {sumElements(m, n)}");
